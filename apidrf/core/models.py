2cd22b4503809e1debbcb235d38585983fc02838
ahslcdev <antoniohenriquedd15@hotmail.com>
2021-10-25 17:43:33 -0300

    config settings, create models and serializer py

--- apidrf/core/models.py
+++ apidrf/core/models.py
@@ -1,3 +1,122 @@
 from django.db import models
+from django.db.models.expressions import F
+from django.contrib.auth.models import BaseUserManager, AbstractUser
 
-# Create your models here.
+
+class BaseManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, username, password, **extrafields):
+        if not username:
+            raise ValueError('Informe o usuário')
+        username = self.model.normalize_username(username)
+        user = self.model(username=username, **extrafields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, password, **extrafields):
+        extrafields.setdefault('is_staff', True)
+        extrafields.setdefault('is_superuser', False)
+        return self._create_user(username, password, **extrafields)
+
+    def create_superuser(self, username, password, **extrafields):
+        extrafields.setdefault('is_staff', True)
+        extrafields.setdefault('is_superuser', True)
+
+        if extrafields.get('is_staff') is not True:
+            raise ValueError('Precisa ser true')
+        if extrafields.get('is_superuser') is not True:
+            raise ValueError('Precisa ser true')
+
+        return self._create_user(username, password, **extrafields)
+
+
+class Created(models.Model):
+    data = models.DateField(auto_now_add=True, blank=True, null=True)
+    hora = models.TimeField(auto_now_add=True, blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Perfil(Created):
+    id = models.AutoField(primary_key=True)
+    perfil = models.CharField(max_length=255, blank=False, null=False)
+
+    class Meta:
+        db_table = 'perfil'
+
+
+class Usuario(AbstractUser):
+    # id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=255, blank=False, null=False)
+    email = models.EmailField(
+        max_length=255, blank=False, null=False, unique=True)
+    status = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['nome', 'email']
+    objects = BaseManager()
+
+    class Meta:
+        db_table = 'usuario'
+
+
+class UsuarioPerfil(Created):
+    id = models.AutoField(primary_key=True)
+    id_usuario = models.ForeignKey(
+        Usuario, on_delete=models.CASCADE, null=True, blank=True)
+    id_perfil = models.ForeignKey(
+        Perfil, on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        db_table = 'usuario_perfil'
+
+
+class Sala(Created):
+    id = models.AutoField(primary_key=True)
+    codigo_sala = models.UUIDField(auto_created=True, blank=True, null=True)
+    entrar = models.BooleanField(default=True, blank=True, null=True)
+    descricao = models.TextField(blank=False, null=False)
+    tags = models.CharField(max_length=500, blank=True, null=True)
+    dono = models.ForeignKey(
+        Usuario, null=False, blank=False, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'sala'
+
+
+class UsuarioSala(Created):
+    id = models.AutoField(primary_key=True)
+    id_usuario = models.ForeignKey(
+        Usuario, on_delete=models.CASCADE, null=True, blank=True)
+    id_sala = models.ForeignKey(
+        Sala, on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        db_table = 'usuario_sala'
+
+
+class Perguntas(Created):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=255, blank=False, null=False)
+    corpo = models.TextField(null=False, blank=False)
+    status = models.BooleanField(default=False)
+    tags = models.CharField(max_length=255, null=True, blank=True)
+
+    class Meta:
+        db_table = 'perguntas'
+
+
+class PerguntaSala(Created):
+    id = models.AutoField(primary_key=True)
+    id_pergunta = models.ForeignKey(
+        Perguntas, on_delete=models.CASCADE, null=True, blank=True)
+    id_sala = models.ForeignKey(
+        Sala, on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        db_table = 'pergunta_sala'
+
+# class Postagem
--- apidrf/projetoii/settings.py
+++ apidrf/projetoii/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -118,8 +122,29 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.Usuario'
+
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
+
+SECURE_HSTS_SECONDS = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_HSTS_PRELOAD = True
+
+SECURE_SSL_REDIRECT = False
